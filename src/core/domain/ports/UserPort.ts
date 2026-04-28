b7f5d0326f8c341c7ef6e3afe80f3f8059e51095
daisy-bait <kadanarpa@gmail.com>
2026-04-27 20:51:30 -0500

    feat(users): add UserPort with the useCases to fetch the users endpoint

---
+++ src/core/domain/ports/UserPort.ts
@@ -0,0 +1,14 @@
+import type { PageResponse } from "../models/common/PaginationModels";
+import type { UserModel } from "../models/users/UserModel";
+
+export interface UserPort {
+    getUserById(userId: string): Promise<UserModel>;
+    pageUsers(page: number, pageSize: number,
+        userName: string,
+        email: string,
+        enable: boolean
+    ): Promise<PageResponse<UserModel>>;
+    registerUser(user: UserModel): Promise<void>;
+    updateUser(user: UserModel): Promise<void>;
+    toggleUserStatus(userId: string, enable: boolean): Promise<void>;
+}
