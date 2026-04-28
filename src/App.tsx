c6c01dea10d017905bd5422662ad306f778fcebb
daisy-bait <kadanarpa@gmail.com>
2026-04-28 16:31:49 -0500

    feat(utils): add taskExecutor

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@
 function App() {
 
   return (
-    <>
+    <div style={{margin: "0 auto", width: "100%", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", fontSize: "2rem"  }}>
       Inkly
-    </>
+    </div>
   )
 }
 
--- src/ui/context/providers/UserProvider.tsx
+++ src/ui/context/providers/UserProvider.tsx
@@ -1,1 +1,24 @@
+import { useContext, useState, type ReactNode } from "react"
+import type { PageResponse } from "../../../core/domain/models/common/PaginationModels";
+import type { UserInfo } from "os";
+
 //// CONTINAUR CON ESTE LOLL; HACER UN PROVIDER PARA EL USUARIO, CON SU INFO, Y FUNCIONES PARA LOGUEAR, DESLOGUEAR, ETC
+type Props = {
+    children: ReactNode;
+}
+
+function UserProvider({ children }: Props) {
+    // Basics
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    //Searching
+    const [pagedUsers, setPagedUsers] = useState<PageResponse<UserInfo> | null>(null);
+    const [searchingUsers, setSearchingUsers] = useState<PageResponse<UserInfo> | null>(null);
+    // Refersh Info per modification
+    const [modifiedUser, setModifiedUser] = useState<boolean>(false);
+
+    // Rest Methods
+
+
+
+}
