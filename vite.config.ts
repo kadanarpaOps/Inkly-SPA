f187aa1cb1a1dcea91636b82f379b351dcd30b90
Julian Enrique <julianbuitragocharry2006@gmail.com>
2026-05-17 22:01:28 -0500

    update vite.config.ts basicSsl qa environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const plugins: any = [react(), tailwindcss()];
+  
+  const needSsl = env.VITE_NEED_LOCAL_SSL === 'true';
+  
+  if (needSsl) {
+    plugins.push(basicSsl());
+  }
+
+  const config: any = {
+    plugins
+  }
+
+  if (needSsl) {
+    config.server = {
+      https: true
+    }
+  }
+
+  return config;
+});
